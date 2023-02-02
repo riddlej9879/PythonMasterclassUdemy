5b5c80e192fd1248680b4c8b880654268e5b77fd
Josh Riddle <riddlej9879@yahoo.com>
2023-02-02 08:26:49 -0600

    Subsec 20 done

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from section_03 import *
 
 
 def print_hi(name):
+    num = 1
     # print(f'Hi, {name}')
     hello_world()
+    for x in range(47):
+        num = num + (num-1)
+        # print("num")
+    print(num)
 
 
 if __name__ == '__main__':
--- section_03.py
+++ section_03.py
@@ -1,12 +1,46 @@
 def hello_world():
+    print("*****Begin Section 03*****")
     # sub 16
-    print('Hello World!')
+    # print('Hello World!')
+
     # Sub 17
-    print(1 + 2)
-    print(7 * 6)
-    print()
-    print("The end", "or is it", "Keep watching to learn more about Python", 3)
+    # print(1 + 2)
+    # print(7 * 6)
+    # print()
+    # print("The end", "or is it", "Keep watching to learn more about Python", 3)
+
     # Sub 18
+    # print("Today is a good day to learn Python")
+    # print("Python is fun")
+    # print("Python's strings are easy to use")
+    # print('We can even include "quotes" in strings')
+    # print("hello" + " world")
+    # greeting = "Hello"
+    # name = input("Please enter name: ")
+    # print(greeting + " " + name)
+
+    # Sub 19
+    # split_str = "This string has been\nsplit over\nseveral\nlines"
+    # print(split_str)
+    # tab_str = "1\t2\t3\t4\t5"
+    # print(tab_str)
+    # print('The per shop owner said "No, no, \'e\'s uh, ... he\'s resting".')
+    # print("The pet show owner said \"No, no, 'e's uh, ... he's resting.\"")
+    # print("""The pet shop owner said "No, no, 'e's uh, ... he's resting".""")
+    # another_str = """This string has been \
+    # split over \
+    # several \
+    # lines"""
+    # print(another_str)
+
+    # Sub 20
+    # print("""The pet shop owner said "No, no, \
+    # 'e's uh, ... he's resting".""")
+    # print("C:\\Users\\notes.txt")
+    # print(r"C:\Users\notes.txt")
+    num = 1
+
+    # Sub 21
 
     print("*****End of Section 03*****")
 
